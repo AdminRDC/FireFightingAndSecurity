a6390ba49f1351b5a8d3ce5c6718edfd4eb1e83e
AdminRDC <chen_2019@qq.com>
2019-12-19 15:38:02 +0800

    提交了部分修改

--- src/pages/data/ShowData.vue
+++ src/pages/data/ShowData.vue
@@ -1,35 +1,35 @@
 
+Use /* eslint-disable */ to ignore all warnings in a file.
 <template>
-  <div class="hello">
-    <div id="chart1" :style="{width:'500px',height:'300px'}"></div>
+  <div class='hello'>
+    <div id='chart1' :style="{width:'500px',height:'300px'}"></div>
   </div>
 </template>
 <script>
 // 引入 ECharts 主模块
-var echarts = require('echarts/lib/echarts');
+var echarts = require('echarts/lib/echarts')
 // 引入柱状图
-require('echarts/lib/chart/bar');
+require('echarts/lib/chart/bar')
 // 引入提示框和标题组件
-require('echarts/lib/component/tooltip');
-require('echarts/lib/component/title');
+require('echarts/lib/component/tooltip')
+require('echarts/lib/component/title')
 export default {
   data() {
-    return {};
+    return {}
   },
   mounted() {
     let that = this
-    this.drawLine();
+    this.drawLine()
   },
-  methods:{
-    drawLine(){
-      /*ECharts图表*/
-      let myChart = this.$echarts.init(document.getElementById("chart1"));
+  methods: {
+    drawLine() {
+      let myChart = this.$echarts.init(document.getElementById('chart1'))
       myChart.setOption({
         series: [
           {
-            name: "访问来源",
-            type: "pie",
-            radius: "55%",
+            name: '访问来源',
+            type: 'pie',
+            radius: '55%',
             itemStyle: {
               normal: {
                 // 阴影的大小
@@ -39,23 +39,24 @@ export default {
                 // 阴影垂直方向上的偏移
                 shadowOffsetY: 0,
                 // 阴影颜色
-                shadowColor: "rgba(0, 0, 0, 0.5)"
+                shadowColor: 'rgba(0, 0, 0, 0.5)'
               }
             },
             data: [
-              { value: 400, name: "搜索引擎" },
-              { value: 335, name: "直接访问" },
-              { value: 310, name: "邮件营销" },
-              { value: 274, name: "联盟广告" },
-              { value: 235, name: "视频广告" }
+              { value: 400, name: '搜索引擎' },
+              { value: 335, name: '直接访问' },
+              { value: 310, name: '邮件营销' },
+              { value: 274, name: '联盟广告' },
+              { value: 235, name: '视频广告' }
             ]
           }
         ]
-      });
+      })
     }
   }
-};
+}
 </script>
 <style scoped>
 
 </style>
+
