1db507d837ea2f9a2420099ce41ea522d775aa62
AdminRDC <chen_2019@qq.com>
2019-12-19 14:33:47 +0800

    提交消防实时数据页面的数据图

--- src/pages/data/ShowData.vue
+++ src/pages/data/ShowData.vue
@@ -1,17 +1,60 @@
+
 <template>
-    <div class="showdata">
-       消防实时数据
-        
-    </div>
+  <div class="hello">
+    <div id="chart1" :style="{width:'500px',height:'300px'}"></div>
+  </div>
 </template>
 <script>
+// 引入 ECharts 主模块
+var echarts = require('echarts/lib/echarts');
+// 引入柱状图
+require('echarts/lib/chart/bar');
+// 引入提示框和标题组件
+require('echarts/lib/component/tooltip');
+require('echarts/lib/component/title');
 export default {
-  data(){
-    return {
-     
-    }
+  data() {
+    return {};
   },
-}
+  mounted() {
+    let that = this
+    this.drawLine();
+  },
+  methods:{
+    drawLine(){
+      /*ECharts图表*/
+      let myChart = this.$echarts.init(document.getElementById("chart1"));
+      myChart.setOption({
+        series: [
+          {
+            name: "访问来源",
+            type: "pie",
+            radius: "55%",
+            itemStyle: {
+              normal: {
+                // 阴影的大小
+                shadowBlur: 200,
+                // 阴影水平方向上的偏移
+                shadowOffsetX: 0,
+                // 阴影垂直方向上的偏移
+                shadowOffsetY: 0,
+                // 阴影颜色
+                shadowColor: "rgba(0, 0, 0, 0.5)"
+              }
+            },
+            data: [
+              { value: 400, name: "搜索引擎" },
+              { value: 335, name: "直接访问" },
+              { value: 310, name: "邮件营销" },
+              { value: 274, name: "联盟广告" },
+              { value: 235, name: "视频广告" }
+            ]
+          }
+        ]
+      });
+    }
+  }
+};
 </script>
 <style scoped>
 
